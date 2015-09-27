527014748284db770c30de8ff44e8eae819fd141
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-27 10:51:21 +0700

    update2

--- list.h
+++ list.h
@@ -2,54 +2,46 @@
 #define LIST_H_INCLUDED
 #include <iostream>
 #include <stdlib.h>
-#include <cstring>
 #include <conio.h>
-
+#include <cstring>
 
 #define nil NULL
-#define info(p) (p)->info
-#define next(p) (p)->next
-#define first(l) ((l).first)
+#define First(L) ((L).First)
+#define Last(L) ((L).Last)
+#define Next(P) P->next
+#define Info(P) P->info
+#define Prev(P) P->prev
 
+using namespace std;
 
 struct infotype
 {
-    char id[4];
-    char nama[15];
-    char warna[25];
-    char jenis[10];
-    char thnklr[20];
-    char namapem[20];
+    char id[4],nama[20],jab[10],umur[5],hobi[15];
 };
 
-typedef struct elmlist *address;
-
-struct elmlist
+typedef struct elemenList *address;
+struct elemenList
 {
     infotype info;
-    address next;
+    address next,prev;
 };
 
-struct list
+struct List
 {
-    address first;
+    address First,Last;
 };
 
-void createlist (list *l);
-void inputdata (infotype *x);
-void dealokasi (address p);
-address alokasi (infotype x);
+void createList(List &);
 
-void insertfirst (list *l, address p);
-void insertlast (list *l, address p);
-void insertafter (list *l, address p, address prec);
+address alokasi(infotype );
+void dealokasi(address &);
 
-void deletefirst (list *l, address *p);
-void deletelast (list *l, address *p);
-void deleteafter (list *l, address *p, address prec);
+void insertFirst(List &, address );
+void insertLast(List &, address );
 
-address searchelement(list l, infotype x);
-void viewlist (list l);
+void deleteFirst(List &, address &);
+void deleteLast(List &, address &);
 
-
+address findElm(List, infotype );
+void printinfo (List );
 #endif // LIST_H_INCLUDED
