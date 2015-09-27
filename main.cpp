d817e2466f6101a6733f5deb644f8f98308c5f1b
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-27 10:55:45 +0700

    update2

--- main.cpp
+++ main.cpp
@@ -3,51 +3,104 @@
 
 using namespace std;
 
+infotype x;
+List l;
+address p;
+int pil;
+
+void inputData ()
+{
+    cout<<"Masukkan ID : ";
+    cin>>x.id;
+    cout<<"Masukkan Nama : ";
+    cin>>x.nama;
+    cout<<"Masukkan Jabatan : ";
+    cin>>x.jab;
+    cout<<"Masukkan Umur : ";
+    cin>>x.umur;
+    cout<<"Masukkan Hobi : ";
+    cin>>x.hobi;
+}
+
+
 int main()
 {
-    address p,prec,q = nil;
-    infotype x;
-    list l;
-    string tes;
-    int pil,pil1;
+   createList(l);
+   do
+   {
+       system("cls");
+       cout<<"Program Double Linked List"<<endl
+           <<"\n1. Insert First"<<endl
+           <<"2. Insert Last"<<endl
+           <<"3. Delete First"<<endl
+           <<"4. Delete Last"<<endl
+           <<"5. View Data"<<endl
+           <<"6  Search Data"<<endl
+           <<"7. Exit"<<endl;
+       cout<<"\nMasukkan Pilihan : ";
+       cin>>pil;
+       switch (pil)
+        {
+        case 1:
+            system("cls");
+            inputData();
+            p = alokasi(x);
+            insertFirst(l,p);
+            cout<<"Insert First Sukses";
+            getch();
+            break;
+        case 2:
+            system("cls");
+            inputData();
+            p = alokasi(x);
+            insertLast(l,p);
+            cout<<"Insert Last Sukses";
+            getch();
+            break;
+        case 3:
+            system("cls");
+            deleteFirst(l,p);
+            dealokasi(p);
+            cout<<"Delete Sukses";
+            getch();
+            break;
+        case 4:
+            system("cls");
+            deleteLast(l,p);
+            dealokasi(p);
+            cout<<"Delete Sukses";
+            getch();
+            break;
+        case 5:
+            system("cls");
+            printinfo(l);
+            getch();
+            break;
+        case 6:
+            system("cls");
+            cout<<"Masukkan ID yang ingin anda cari : ";
+            cin>>x.id;
+            p = findElm(l,x);
+            if (p != nil)
+            {
+                cout<<"\nNama    : "<<Info(p).nama<<endl
+                    <<"Jabatan : "<<Info(p).jab<<endl
+                    <<"Umur    : "<<Info(p).umur<<endl
+                    <<"Hobi    : "<<Info(p).hobi<<endl;
+            }
+            else
+            {
+                cout<<"ID tidak ada dalam list";
+            }
+            getch();
+            break;
+        case 7:
+            break;
+        default :
+            cout<<"Invalid Input";
+            getch();
+        }
+   }
+   while (pil != 7);
+}
 
-    createlist(&l);
-
-    inputdata(&x);
-    p = alokasi(x);
-    insertfirst(&l,p);
-
-    cout<<endl;
-    inputdata(&x);
-    p = alokasi(x);
-    insertlast(&l,p);
-
-    viewlist(l);
-
-    cout<<endl;
-    cout<<endl;
-    strcpy(x.id,"x01");
-    p = searchelement(l,x);
-    cout<<info(p).nama<<endl;
-
-    deletefirst(&l,&p);
-    cout<<endl;
-    viewlist(l);
-
-    cout<<endl;
-    strcpy(x.id,"x01");
-    q = searchelement(l,x);
-    cout<<endl;
-    cout<<endl;
-    if (q == nil)
-    {
-        cout<<"NULL"<<endl;
-    }
-    else
-    {
-        cout<<q;
-    }
-
-
-    getch();
-    return 0;
