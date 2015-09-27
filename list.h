b605164f0f87efb351c7bac5eeabec1acb8715c2
sizoune <muhammadwildaniskandar@gmail.com>
2015-09-27 19:12:20 +0700

    Update list.h

--- list.h
+++ list.h
@@ -8,15 +8,19 @@
 #define nil NULL
 #define First(L) ((L).First)
 #define Last(L) ((L).Last)
-#define Next(P) P->next
-#define Info(P) P->info
-#define Prev(P) P->prev
+#define Next(P) (P)->next
+#define Info(P) (P)->info
+#define Prev(P) (P)->prev
 
 using namespace std;
 
 struct infotype
 {
-    char id[4],nama[20],jab[10],umur[5],hobi[15];
+    char id[10];
+    char nm[25];
+    char jb[15];
+    char um[10];
+    char hb[18];
 };
 
 typedef struct elemenList *address;
@@ -31,17 +35,18 @@ struct List
     address First,Last;
 };
 
-void createList(List &);
+void createList(List *L);
 
-address alokasi(infotype );
-void dealokasi(address &);
+address alokasi(infotype x);
+void dealokasi(address *p);
 
-void insertFirst(List &, address );
-void insertLast(List &, address );
+void insertFirst(List *L, address p);
+void insertLast(List *L, address p);
 
-void deleteFirst(List &, address &);
-void deleteLast(List &, address &);
+void deleteFirst(List *L, address *p);
+void deleteLast(List *L, address *p);
 
-address findElm(List, infotype );
-void printinfo (List );
+address findElm(List L, infotype p);
+void printinfo (List L);
+
 #endif // LIST_H_INCLUDED
